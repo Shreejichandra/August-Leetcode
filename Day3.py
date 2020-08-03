5d0b9c6bf6c6b014382c78fd3fe02154dcc10d9c
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-08-03 22:20:36 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,15 @@
+//Q3 Valid Pallindrome
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        sh = s.lower()
+        new = ""
+        for i in sh:
+            if ((i >= 'a' and i <= 'z') or (i >= '0' and i <= '9')):
+                new += i
+        if new == new[::-1]:
+            return True
+        else:
+            return False
+        
+        
