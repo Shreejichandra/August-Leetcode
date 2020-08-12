a927dfec5eb7fc8c746d0cd556a810298f0eddbf
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-08-13 01:46:26 +0530

    Create Day12.py

---
+++ Day12.py
@@ -0,0 +1,14 @@
+#Pascal's Triangle II
+
+class Solution:
+    def getRow(self, rowIndex):
+        elements_in_rows = []
+        for i in range(0, rowIndex+1):
+            elements_in_rows.append([])
+            elements_in_rows[i].append(1)
+            for j in range(1,i):
+                elements_in_rows[i].append(elements_in_rows[i-1][j-1]+elements_in_rows[i-1][j])
+            if i!=0:    
+                elements_in_rows[i].append(1)
+        
+        return elements_in_rows[rowIndex]  
