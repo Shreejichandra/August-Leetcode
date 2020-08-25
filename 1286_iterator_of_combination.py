7acb5ecf9d782703f9d460da82d95688013e9482
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-08-25 14:51:43 +0530

    Added the question statement

--- 1286_iterator_of_combination.py
+++ 1286_iterator_of_combination.py
@@ -1,4 +1,9 @@
-# Iterator for Combination
+'''Design an Iterator class, which has:
+
+A constructor that takes a string characters of sorted distinct lowercase English letters and a number combinationLength as arguments.
+A function next() that returns the next combination of length combinationLength in lexicographical order.
+A function hasNext() that returns True if and only if there exists a next combination.
+ '''
 from itertools import combinations
 
 class CombinationIterator:
