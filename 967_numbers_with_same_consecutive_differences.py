0e9fc650f37c4f3adf2da4bbae5bbd3e12efe370
Shreeji <55141517+Shreejichandra@users.noreply.github.com>
2020-08-29 01:37:32 +0530

    Added the question statement

--- 967_numbers_with_same_consecutive_differences.py
+++ 967_numbers_with_same_consecutive_differences.py
@@ -1,4 +1,8 @@
-#Numbers With Same Consecutive Differences
+'''Return all non-negative integers of length N such that the absolute difference between every two consecutive digits is K.
+
+Note that every number in the answer must not have leading zeros except for the number 0 itself. For example, 01 has one leading zero and is invalid, but 0 is valid.
+
+You may return the answer in any order.'''
 
 
 class Solution:
